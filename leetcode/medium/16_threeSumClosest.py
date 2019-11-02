b1033e6103d3c9a6fda23a68e62319664d21de9e
zwb19880917@163.com <wbzhu@21fucker.local>
2019-11-02 22:21:27 +0800

    leetcode

---
+++ leetcode/medium/16_threeSumClosest.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+
+'''
+
+先排序, 然后遍历, 然后内部使用双指针, 时间复杂度应该是O(n²)
+
+'''
+
+ # https://leetcode-cn.com/problems/3sum-closest
+
+import sys
+
+class Solution(object):
+  def threeSumClosest(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    nums.sort()
+
+    min_gap = sys.maxint
+    min_sum = 0
+    for i in range(len(nums)):
+      s = i+1
+      e = len(nums)-1
+      while s < e:
+        three_sum = nums[i] + nums[s] + nums[e]
+        gap = abs(target - three_sum)
+        if gap < min_gap:
+          min_gap = gap
+          min_sum = three_sum
+        if three_sum == target:
+          return three_sum
+        if three_sum < target:
+          s += 1
+        else:
+          e += -1
+    return min_sum
