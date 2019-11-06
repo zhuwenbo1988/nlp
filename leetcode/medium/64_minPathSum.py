61e600475410b8984f00f9d3caaa7d1742020f75
wbzhu <wbzhu@mobvoi.com>
2019-11-06 19:06:42 +0800

    leetcode

---
+++ leetcode/medium/64_minPathSum.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+# https://leetcode-cn.com/problems/minimum-path-sum
+
+class Solution(object):
+  def minPathSum(self, grid):
+    """
+    :type grid: List[List[int]]
+    :rtype: int
+    """
+    m = len(grid)
+    n = len(grid[0])
+    matrix = [[0] * n for i in range(m)]
+    for i in range(m):
+      for j in range(n):
+        if i == 0 and j == 0:
+          matrix[i][j] = grid[i][j]
+        else:
+          if i-1 < 0:
+            matrix[i][j] = grid[i][j] + matrix[i][j-1]
+          elif j-1 < 0:
+            matrix[i][j] = grid[i][j] + matrix[i-1][j]
+          else:
+            matrix[i][j] = min(grid[i][j] + matrix[i-1][j], grid[i][j] + matrix[i][j-1])
+    return matrix[m-1][n-1]
