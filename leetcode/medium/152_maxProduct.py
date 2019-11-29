52e3aeace3dc447a3449e7cb71cd48db024272be
wbzhu <wbzhu@mobvoi.com>
2019-11-29 17:56:32 +0800

    leetcode

---
+++ leetcode/medium/152_maxProduct.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+
+'''
+
+动态规划
+
+我们只要记录前i的最小值, 和最大值, 那么 dp[i] = max(nums[i] * pre_max, nums[i] * pre_min, nums[i]), 这里0 不需要单独考虑, 因为当相乘不管最大值和最小值,都会置0
+
+'''
+
+# https://leetcode-cn.com/problems/maximum-product-subarray/
+
+class Solution(object):
+  def maxProduct(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    n = len(nums)
+    max_l = [0] * n
+    min_l = [0] * n
+    max_l[0] = nums[0]
+    min_l[0] = nums[0]
+    result = nums[0]
+
+    for i in range(1, n):
+      a = max_l[i-1] * nums[i]
+      b = max(min_l[i-1] * nums[i], nums[i])
+      max_l[i] = max(a, b)
+      a = max_l[i-1] * nums[i]
+      b = min(min_l[i-1] * nums[i], nums[i])
+      min_l[i] = min(a, b)
+      result = max(result, max_l[i])
+    return result
