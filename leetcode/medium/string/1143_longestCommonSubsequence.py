2b48f72d8dfa8d229b925a78a81226b31d509a9c
zwb19880917@163.com <wbzhu@21fucker.local>
2020-01-19 23:58:52 +0800

    leetcode

---
+++ leetcode/medium/string/1143_longestCommonSubsequence.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+'''
+
+非常经典的二维动态规划
+
+'''
+
+# https://leetcode-cn.com/problems/longest-common-subsequence/
+
+class Solution(object):
+  def longestCommonSubsequence(self, text1, text2):
+    """
+    :type text1: str
+    :type text2: str
+    :rtype: int
+    """
+    m = len(text1)
+    n = len(text2)
+    dp = [[0] * (n+1) for i in range(m+1)]
+    for i in range(1, m+1):
+      for j in range(1, n+1):
+        if text1[i-1] == text2[j-1]:
+          dp[i][j] = 1 + dp[i-1][j-1]
+        else:
+          dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    return dp[m][n]
