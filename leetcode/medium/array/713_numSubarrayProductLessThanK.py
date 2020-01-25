058521388202f33d9ab1122dd3fee210ee345e9a
wbzhu <wbzhu@mobvoi.com>
2020-01-25 15:53:58 +0800

    leetcode

---
+++ leetcode/medium/array/713_numSubarrayProductLessThanK.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+'''
+
+双指针
+
+'''
+
+# https://leetcode-cn.com/problems/subarray-product-less-than-k/
+
+class Solution(object):
+  def numSubarrayProductLessThanK(self, nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+    if k <= 1:
+      return 0
+    j = 0
+    n = len(nums)
+    result = 0
+    curr_p = 1
+    for i in range(n):
+      curr_p *= nums[i]
+      while curr_p >= k:
+        curr_p /= nums[j]
+        j += 1
+      # 关键
+      result += i-j+1
+    return result
