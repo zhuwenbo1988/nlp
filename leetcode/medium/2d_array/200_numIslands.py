98fb48fab543d8bc575b6798cb1893db80c443a3
wbzhu <wbzhu@mobvoi.com>
2020-01-27 22:18:00 +0800

    leetdoe

---
+++ leetcode/medium/2d_array/200_numIslands.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+'''
+
+深度优先遍历
+
+线性扫描整个二维网格，如果一个结点包含1，则以其为根结点启动深度优先搜索。在深度优先搜索过程中，每个访问过的结点被标记为0。计数启动深度优先搜索的根结点的数量，即为岛屿的数量。
+
+'''
+
+# https://leetcode-cn.com/problems/number-of-islands/
+
+class Solution(object):
+  def numIslands(self, grid):
+    """
+    :type grid: List[List[str]]
+    :rtype: int
+    """
+    def dfs(grid, row, col):
+      if row < 0 or col < 0:
+        return
+      if row >= len(grid) or col >= len(grid[0]):
+        return
+      if grid[row][col] == '0':
+        return
+
+      grid[row][col] = '0'
+    
+      dfs(grid, row-1, col)
+      dfs(grid, row+1, col)
+      dfs(grid, row, col-1)
+      dfs(grid, row, col+1)
+    if not grid:
+      return 0
+    m = len(grid)
+    n = len(grid[0])
+    result = 0
+    for i in range(m):
+      for j in range(n):
+        if grid[i][j] == '0':
+          continue
+        result += 1
+        dfs(grid, i, j)
+    return result
